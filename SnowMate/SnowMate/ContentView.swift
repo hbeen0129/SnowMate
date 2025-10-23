f93181959ab019d3e5612db60202790f9bb8e8f6
HyebeenLee(Emma) <89961823+hbeen0129@users.noreply.github.com>
2025-10-24 00:22:14 +0900

    [Feat] #1 워치 앱 타겟 추가 하고 빌드 해보기

---
+++ SnowMate/SnowMate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SnowMate
+//
+//  Created by 이혜빈 on 10/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
